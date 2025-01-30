9f3d5023550b994ce70db97b5db711062564a259
coloryourlife <95chris0319@gmail.com>
2025-01-29 17:56:16 -0800

    define endpoint for lora adapter using OpenAIServingModels in vLLM

--- llama_service.py
+++ llama_service.py
@@ -3,7 +3,13 @@ import os
 from typing import Dict, Optional, List
 import logging
 
+from dataclasses import dataclass
+
+from argparse import Namespace
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
+from fastapi.responses import Response
 from starlette.requests import Request
 from starlette.responses import StreamingResponse, JSONResponse
 
@@ -17,8 +23,11 @@ from vllm.entrypoints.openai.protocol import (
     ChatCompletionResponse,
     ErrorResponse,
 )
+from vllm.entrypoints.openai.serving_models import (BaseModelPath,
+                                                    OpenAIServingModels)
 from vllm.entrypoints.openai.serving_chat import OpenAIServingChat
 from vllm.entrypoints.openai.serving_engine import LoRAModulePath
+from vllm.entrypoints.openai.protocol import LoadLoraAdapterRequest, UnloadLoraAdapterRequest
 from vllm.utils import FlexibleArgumentParser
 
 logger = logging.getLogger("ray.serve")
@@ -29,45 +38,61 @@ app = FastAPI()
 @serve.deployment(name="VLLMDeployment")
 @serve.ingress(app)
 class VLLMDeployment:
-    def __init__(
+    async def __ainit__(
         self,
+        model: str,
         engine_args: AsyncEngineArgs,
         response_role: str,
-        lora_modules: Optional[List[LoRAModulePath]] = None,
         chat_template: Optional[str] = None,
     ):
         logger.info(f"Starting with engine args: {engine_args}")
-        self.openai_serving_chat = None
         self.engine_args = engine_args
-        self.response_role = response_role
-        self.lora_modules = lora_modules
-        self.chat_template = chat_template
         self.engine = AsyncLLMEngine.from_engine_args(engine_args)
+        self.model_config = self.engine.get_model_config()
+        self.openai_serving_models = OpenAIServingModels(
+            engine_client=self.engine,
+            model_config=self.model_config,
+            base_model_paths=[BaseModelPath(name=model, model_path=model)],            # BaseModelPath(name=name, model_path=args.model)
+            lora_modules=None,              # If we are loading lora module when initiate app
+        )
+        await self.openai_serving_models.init_static_loras()
+        self.openai_serving_chat = OpenAIServingChat(
+            engine_client=self.engine,
+            model_config=self.model_config,
+            models=self.openai_serving_models,
+            response_role=response_role,
+            chat_template=chat_template,
+        )
+        self.response_role = response_role
+        self.chat_template = chat_template
+
+    @app.post("/v1/load_lora_adapter")
+    async def load_lora_adapter(self,
+                                request: LoadLoraAdapterRequest,
+                                raw_request: Request):
+        response = await self.openai_serving_models.load_lora_adapter(request)
+        if isinstance(response, ErrorResponse):
+            return JSONResponse(content=response.model_dump(),
+                                status_code=response.code)
+
+        return Response(status_code=200, content=response)
+
+    @app.post("/v1/unload_lora_adapter")
+    async def unload_lora_adapter(self,
+                                  request: UnloadLoraAdapterRequest,
+                                  raw_request: Request):
+
+        response = await self.openai_serving_models.unload_lora_adapter(request)
+        if isinstance(response, ErrorResponse):
+            return JSONResponse(content=response.model_dump(),
+                                status_code=response.code)
+
+        return Response(status_code=200, content=response)
 
     @app.post("/v1/chat/completions")
     async def create_chat_completion(
         self, request: ChatCompletionRequest, raw_request: Request
     ):
-        """OpenAI-compatible HTTP endpoint
-        API reference:
-            - https://docs.vllm.ai/en/latest/serving/openai_compatible_server.html
-        """
-        if not self.openai_serving_chat:
-            model_config = await self.engine.get_model_config()
-            if self.engine_args.served_model_name is not None:
-                served_model_names = self.engine_args.served_model_name
-            else:
-                served_model_names = [self.engine_args.model]
-            self.openai_serving_chat = OpenAIServingChat(
-                self.engine,
-                model_config,
-                served_model_names=served_model_names,
-                response_role=self.response_role,
-                lora_modules=self.lora_modules,
-                chat_template=self.chat_template,
-                prompt_adapters=None,
-                request_logger=None,
-            )
         logger.info(f"Request: {request}")
         generator = await self.openai_serving_chat.create_chat_completion(
             request, raw_request
@@ -100,15 +125,25 @@ def build_app(cli_args: Dict[str, str]) -> serve.Application:
     engine_args.worker_use_ray = True
 
     return VLLMDeployment.bind(
+        parsed_args.model,
         engine_args,
         parsed_args.response_role,
-        parsed_args.lora_modules,
         parsed_args.chat_template,
     )
 
 
 model = build_app({
-    "model": os.environ['MODEL_ID'],
+    "model": "meta-llama/Llama-3.2-1B-Instruct",
+    "max-lora-rank": "32",
+    "enable-lora": "true",
+    # "lora-modules": {
+    #     "name": os.environ['DEPLOYMENT_ID'],  # lora_integer_id (globally unique)
+    #     "path": os.environ['LORA_PATH'],
+    #     "base_model_name": "meta-llama/Llama-3.2-1B-Instruct",
+    # },
     "tensor-parallel-size": os.environ['TENSOR_PARALLELISM'],
     "pipeline-parallel-size": os.environ['PIPELINE_PARALLELISM']
 })
+
+# vllm serve meta-llama/Llama-3.2-1B-Instruct --enable-lora --lora-modules '{"name": "tr-CQjJeCUnYtuTT8g3yp6zu2-3",
+# "path": "/home/ubuntu/artifacts", "base_model_name": "meta-llama/Llama-3.2-1B-Instruct"}' --max-lora-rank 32
